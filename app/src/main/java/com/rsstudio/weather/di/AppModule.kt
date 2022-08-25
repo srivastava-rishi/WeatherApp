6e5c4290e2415a90ff0975c6e150fe0405d626f3
Rishi Srivastava <official.rishisrivastava@example.com>
2022-08-25 23:01:59 +0530

    hilt setup

---
+++ app/src/main/java/com/rsstudio/weather/di/AppModule.kt
@@ -0,0 +1,22 @@
+package com.rsstudio.weather.di
+
+import android.content.Context
+import com.rsstudio.weather.app.App
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun applicationContext( @ApplicationContext applicationContext: Context) : App {
+        return applicationContext as App
+    }
+
+}
--- app/src/main/java/com/rsstudio/weather/ui/base/BaseActivity.kt
+++ app/src/main/java/com/rsstudio/weather/ui/base/BaseActivity.kt
@@ -1,10 +1,12 @@
 package com.rsstudio.weather.ui.base
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.rsstudio.weather.R
 import com.rsstudio.weather.databinding.ActivityBaseBinding
+import io.github.inflationx.viewpump.ViewPumpContextWrapper
 
 abstract class BaseActivity : AppCompatActivity() {
 
@@ -15,5 +17,8 @@ abstract class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_base)
+    }
+    override fun attachBaseContext(context: Context) {
+        super.attachBaseContext(ViewPumpContextWrapper.wrap(context))
     }
 }
