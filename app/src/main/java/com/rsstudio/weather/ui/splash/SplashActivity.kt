2a6c7348e248eb58baee72425496f7d5fc60b116
Rishi Srivastava <official.rishisrivastava@example.com>
2022-08-26 02:55:31 +0530

    ask permission logic done

--- app/src/main/java/com/rsstudio/weather/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/rsstudio/weather/ui/splash/SplashActivity.kt
@@ -1,25 +1,37 @@
 package com.rsstudio.weather.ui.splash
 
+import android.Manifest
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.rsstudio.weather.R
 import com.rsstudio.weather.databinding.ActivitySplashBinding
 import com.rsstudio.weather.ui.base.BaseActivity
 import com.rsstudio.weather.ui.main.MainActivity
+import com.vmadalin.easypermissions.EasyPermissions
+import com.vmadalin.easypermissions.dialogs.SettingsDialog
 
-class SplashActivity : BaseActivity() {
+class SplashActivity : BaseActivity() , EasyPermissions.PermissionCallbacks {
 
     private lateinit var binding: ActivitySplashBinding
+
+    companion object {
+        const val PERMISSION_LOCATION_REQUEST_CODE = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
 
         //
-        gotoMainActivity()
+        if (hasLocationPermission()) {
+            gotoMainActivity()
+        } else {
+            requestLocationPermission()
+        }
     }
 
     private fun gotoMainActivity() {
@@ -31,6 +43,45 @@ class SplashActivity : BaseActivity() {
             }, 500
         )
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    private fun hasLocationPermission() =
+        EasyPermissions.hasPermissions(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+
+    private fun requestLocationPermission() {
+        EasyPermissions.requestPermissions(
+            this,
+            "This application cannot work without Location Permission.",
+            PERMISSION_LOCATION_REQUEST_CODE,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    }
+
+
+    override fun onPermissionsDenied(requestCode: Int, perms: List<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            SettingsDialog.Builder(this).build().show()
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {
+        //
+        gotoMainActivity()
+    }
+
 
 
 }
